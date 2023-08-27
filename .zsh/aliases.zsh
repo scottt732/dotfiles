6edea4b3f71865c758f2d4d7a769f67a7f2adf7f
Scott Holodak <scottt732@gmail.com>
2023-08-27 19:15:31 -0400

    lsd

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -17,7 +17,13 @@ fi
 
 alias git-remote-keybase="git-remote-keybase.exe"
 
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='lt --tree'
 alias fzf="f() { /usr/bin/fzf --preview \"bat --color=always --style=numbers --line-range=:500 {}\" $@};f"
 alias findex="f() { find $@ -exec bat {} + };f"
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
 function tail() { tail $@ | bat --paging=never -l log }
