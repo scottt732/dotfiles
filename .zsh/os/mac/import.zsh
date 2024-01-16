44d68be950117e581191199321a88f6eb2ac3cb9
Scott Holodak <scottt732@gmail.com>
2024-01-16 16:10:50 -0500

    iterm2 integration

--- .zsh/os/mac/import.zsh
+++ .zsh/os/mac/import.zsh
@@ -1,2 +1,3 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
